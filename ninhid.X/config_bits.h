f677ceb9983512513f371acdb62bbc1e6f441861
Frank Razenberg <frank@zzattack.org>
2015-05-24 16:45:18 +0200

    headers

---
+++ ninhid.X/config_bits.h
@@ -0,0 +1,64 @@
+#ifndef CONFIG_BITS_H
+#define	CONFIG_BITS_H
+
+#include <xc.h>
+
+// CONFIG1L
+#pragma config PLLSEL = PLL3X   // PLL Selection (3x clock multiplier)
+#pragma config CFGPLLEN = OFF   // PLL Enable Configuration bit (PLL Disabled (firmware controlled))
+#pragma config CPUDIV = NOCLKDIV// CPU System Clock Postscaler (CPU uses system clock (no divide))
+#pragma config LS48MHZ = SYS48X8// Low Speed USB mode with 48 MHz system clock (System clock at 48 MHz, USB clock divider is set to 8)
+
+// CONFIG1H
+#pragma config FOSC = INTOSCIO  // Oscillator Selection (Internal oscillator)
+#pragma config PCLKEN = OFF     // Primary Oscillator Shutdown (Primary oscillator shutdown firmware controlled)
+#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF       // Internal/External Oscillator Switchover (Oscillator Switchover mode disabled)
+
+// CONFIG2L
+#pragma config nPWRTEN = OFF    // Power-up Timer Enable (Power up timer disabled)
+#pragma config BOREN = SBORDIS  // Brown-out Reset Enable (BOR enabled in hardware (SBOREN is ignored))
+#pragma config BORV = 190       // Brown-out Reset Voltage (BOR set to 1.9V nominal)
+#pragma config nLPBOR = ON      // Low-Power Brown-out Reset (Low-Power Brown-out Reset enabled)
+
+// CONFIG2H
+#pragma config WDTEN = OFF     // Watchdog Timer Enable bits (WDT controlled by firmware (SWDTEN enabled))
+#pragma config WDTPS = 32768    // Watchdog Timer Postscaler (1:32768)
+
+// CONFIG3H
+#pragma config CCP2MX = RC1     // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<5:0> pins are configured as digital I/O on Reset)
+#pragma config T3CMX = RC0      // Timer3 Clock Input MUX bit (T3CKI function is on RC0)
+#pragma config SDOMX = RC7      // SDO Output MUX bit (SDO function is on RC7)
+#pragma config MCLRE = ON       // Master Clear Reset Pin Enable (MCLR pin enabled; RE3 input disabled)
+
+// CONFIG4L
+#pragma config STVREN = ON      // Stack Full/Underflow Reset (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF        // Single-Supply ICSP Enable bit (Single-Supply ICSP disabled)
+#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled)
+
+// CONFIG5L
+#pragma config CP0 = OFF        // Block 0 Code Protect (Block 0 is not code-protected)
+#pragma config CP1 = OFF        // Block 1 Code Protect (Block 1 is not code-protected)
+
+// CONFIG5H
+#pragma config CPB = OFF        // Boot Block Code Protect (Boot block is not code-protected)
+#pragma config CPD = OFF        // Data EEPROM Code Protect (Data EEPROM is not code-protected)
+
+// CONFIG6L
+#pragma config WRT0 = OFF       // Block 0 Write Protect (Block 0 (0800-1FFFh) is not write-protected)
+#pragma config WRT1 = OFF       // Block 1 Write Protect (Block 1 (2000-3FFFh) is not write-protected)
+
+// CONFIG6H
+#pragma config WRTC = OFF       // Configuration Registers Write Protect (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB = OFF       // Boot Block Write Protect (Boot block (0000-7FFh) is not write-protected)
+#pragma config WRTD = OFF       // Data EEPROM Write Protect (Data EEPROM is not write-protected)
+
+// CONFIG7L
+#pragma config EBTR0 = OFF      // Block 0 Table Read Protect (Block 0 is not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF      // Block 1 Table Read Protect (Block 1 is not protected from table reads executed in other blocks)
+
+// CONFIG7H
+#pragma config EBTRB = OFF      // Boot Block Table Read Protect (Boot block is not protected from table reads executed in other blocks)
+
+#endif	/* CONFIG_BITS_H */
