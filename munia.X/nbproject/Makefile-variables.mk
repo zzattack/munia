de28a0039f0418cd401318f2e5faebab88c8f902
Frank Razenberg <frank@zzattack.org>
2015-07-20 22:52:29 +0200

    rename project

---
+++ munia.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# release configuration
+CND_ARTIFACT_DIR_release=dist/release/production
+CND_ARTIFACT_NAME_release=ninhid.X.production.hex
+CND_ARTIFACT_PATH_release=dist/release/production/ninhid.X.production.hex
+CND_PACKAGE_DIR_release=${CND_DISTDIR}/release/package
+CND_PACKAGE_NAME_release=ninhid.x.tar
+CND_PACKAGE_PATH_release=${CND_DISTDIR}/release/package/ninhid.x.tar
+# debug configuration
+CND_ARTIFACT_DIR_debug=dist/debug/production
+CND_ARTIFACT_NAME_debug=ninhid.X.production.hex
+CND_ARTIFACT_PATH_debug=dist/debug/production/ninhid.X.production.hex
+CND_PACKAGE_DIR_debug=${CND_DISTDIR}/debug/package
+CND_PACKAGE_NAME_debug=ninhid.x.tar
+CND_PACKAGE_PATH_debug=${CND_DISTDIR}/debug/package/ninhid.x.tar
