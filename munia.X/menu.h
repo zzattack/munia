90ee1d238c694373108e8560970cd9398111ad89
Frank Razenberg <frank@zzattack.org>
2015-10-29 22:50:00 +0100

    update settings names

--- munia.X/menu.h
+++ munia.X/menu.h
@@ -16,15 +16,16 @@ enum __menu_command {
     next_page, prev_page, left, right, confirm, cancel, exit,
 };
 
+// this is where the input is redirected to
 const char* menu_sub_items[][4] = {
-    {"NGC ", "PC ", NULL}, // MENU_PAGE_NGC,
+    {"NGC ", "PC ", NULL},         // MENU_PAGE_NGC,
     {"N64 ", "PC ", NULL },        // MENU_PAGE_N64,
     {"SNES", "PC ", "NGC", NULL }, // MENU_PAGE_SNES,
     {"A:Ok ", " B:Cancel", NULL }, // MENU_PAGE_EXIT,
 };
 enum __snes_modes { SNES_MODE_SNES, SNES_MODE_PC, SNES_MODE_NGC };
-enum __n64_modes  { N64_MODE_SNES,  N64_MODE_PC,  N64_MODE_NGC  };
-enum __ngc_modes  { NGC_MODE_NGC,   NGC_MODE_PC,  NGC_MODE_SNES };
+enum __n64_modes  { N64_MODE_N64,   N64_MODE_PC  };
+enum __ngc_modes  { NGC_MODE_NGC,   NGC_MODE_PC };
 
 typedef struct {
     int8_t snes_mode;
