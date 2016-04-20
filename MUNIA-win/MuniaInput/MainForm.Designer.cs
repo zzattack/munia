a41d2c58f7630525f6a9bebfa6c15714a3010485
Frank Razenberg <frank@zzattack.org>
2016-04-20 14:42:46 +0200

    commit input tester project

---
+++ MUNIA-win/MuniaInput/MainForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace MuniaInput {
+    partial class MainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(53, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(201, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "dev";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 71);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(419, 233);
+            this.textBox1.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 316);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ MUNIA-win/MuniaInput/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Resources;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using SharpLib.Hid;
+using SharpLib.Win32;
+
+namespace MuniaInput {
+    public partial class MainForm : Form {
+        MuniaController _activeController;
+        
+        public delegate void OnHidEventDelegate(object aSender, Event aHidEvent);
+
+        public MainForm() {
+            InitializeComponent();
+
+            comboBox1.Items.AddRange(MuniaController.ListDevices().ToArray());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, System.EventArgs e) {
+            Register(comboBox1.SelectedItem as MuniaController);
+        }
+
+        private void Register(MuniaController c) {
+            _activeController = c;
+            c.Activate(this.Handle);
+            c.StateUpdated += ControllerStateUpdated;
+        }
+
+        private void ControllerStateUpdated(object sender, EventArgs args) {
+            if (InvokeRequired) Invoke((Action<object, EventArgs>)ControllerStateUpdated, sender, args);
+            else {
+                textBox1.AppendText("Buttons: ");
+                textBox1.AppendText(string.Join("", _activeController.Buttons.Select(x => x ? "Y" : "N")));
+                textBox1.AppendText("\r\nAxes:");
+                textBox1.AppendText(string.Join("  ", _activeController.Axes));
+                textBox1.AppendText("\r\n");
+            }
+        }
+
+        /// <summary>
+        /// Hook in HID handler.
+        /// </summary>
+        /// <param name="message"></param>
+        protected override void WndProc(ref Message message) {
+            switch (message.Msg) {
+                case Const.WM_INPUT:
+                    // Returning zero means we processed that message
+                    message.Result = IntPtr.Zero;
+                    _activeController?.WndProc(ref message);
+                break;
+            }
+            base.WndProc(ref message);
+        }
+
+    }
+
+}
