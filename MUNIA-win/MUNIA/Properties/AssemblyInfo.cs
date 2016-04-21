8f55b3283f1105bc9f275c10e3b7db494601e4ce
Frank Razenberg <frank@zzattack.org>
2016-04-22 01:45:25 +0200

    auto increment settings

--- MUNIA-win/MUNIA/Properties/AssemblyInfo.cs
+++ MUNIA-win/MUNIA/Properties/AssemblyInfo.cs
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("278dcc56-806a-4749-a39e-8140bc80d340")]
+[assembly: Guid("278dcc56-806a-4749-a39e-8140bc80d341")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
