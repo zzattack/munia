f1baab5eb5cdc642ba67a85b3fce0f87dbc8a9cd
Frank Razenberg <frank@zzattack.org>
2016-04-24 20:45:35 +0200

    version bump

--- MUNIA-win/MUNIA/Properties/VersionInfo.cs
+++ MUNIA-win/MUNIA/Properties/VersionInfo.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("0.9")]
-[assembly: System.Reflection.AssemblyFileVersion("0.9.114.20")]
+[assembly: System.Reflection.AssemblyFileVersion("0.9.114.113")]
 
 
