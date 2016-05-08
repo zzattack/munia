1e670a91e512e174adf0aa6fa5aeeeed883845ba
Frank Razenberg <frank@zzattack.org>
2016-05-08 21:50:14 +0200

    Improve USB notification class

--- MUNIA-win/MUNIA/UsbNotification.cs
+++ MUNIA-win/MUNIA/UsbNotification.cs
@@ -1,26 +1,26 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
 
 namespace MUNIA {
 
 	public static class UsbNotification {
-		private static readonly Guid GuidUsbDevices = new Guid("A5DCBF10-6530-11D2-901F-00C04FB951ED"); // USB devices
+		public static readonly Guid GUID_DEVINTERFACE_USB_DEVICE = new Guid("A5DCBF10-6530-11D2-901F-00C04FB951ED"); // USB Raw Device.
+		public static readonly Guid GUID_DEVINTERFACE_HID = new Guid("4d1e55b2-f16f-11cf-88cb-001111000030"); // USB HID Device.
+		
 		private static IntPtr _notificationHandle;
-		private const int DbtDevtypDeviceinterface = 5;
-		private static UsbDeviceNotificationListenerWindow _window;
+		private static readonly UsbDeviceNotificationListenerWindow _window;
 
 		public static event EventHandler<UsbNotificationEventArgs> DeviceArrival;
 		public static event EventHandler<UsbNotificationEventArgs> DeviceRemovalComplete;
 
 		private static void OnDeviceArrival(UsbNotificationEventArgs args) {
-			if (DeviceArrival != null)
-				DeviceArrival(null, args);
+			DeviceArrival?.Invoke(null, args);
 		}
 
 		private static void OnDeviceRemoveComplete(UsbNotificationEventArgs args) {
-			if (DeviceRemovalComplete != null)
-				DeviceRemovalComplete(null, args);
+			DeviceRemovalComplete?.Invoke(null, args);
 		}
 
 		static UsbNotification() {
@@ -38,17 +38,19 @@ namespace MUNIA {
 		/// <param name="windowHandle">Handle to the window receiving notifications.</param>
 		private static void RegisterUsbDeviceNotification(IntPtr windowHandle) {
 			var dbi = new DevBroadcastDeviceinterface {
-				DeviceType = DbtDevtypDeviceinterface,
+				DeviceType = DeviceType.DBT_DEVTYP_DEVICEINTERFACE,
 				Reserved = 0,
-				ClassGuid = GuidUsbDevices,
-				Name = 0
+				Name = 0,
+				ClassGuid = GUID_DEVINTERFACE_HID, // not used when flags contains ALL_INTERFACE_CLASSES
 			};
 
 			dbi.Size = Marshal.SizeOf(dbi);
 			IntPtr buffer = Marshal.AllocHGlobal(dbi.Size);
 			Marshal.StructureToPtr(dbi, buffer, true);
 
-			_notificationHandle = RegisterDeviceNotification(windowHandle, buffer, 0);
+			RegisterDeviceNotificationFlags flags =
+				RegisterDeviceNotificationFlags.DEVICE_NOTIFY_WINDOW_HANDLE;
+			_notificationHandle = RegisterDeviceNotification(windowHandle, buffer, flags);
 		}
 
 		/// <summary>
@@ -59,7 +61,7 @@ namespace MUNIA {
 		}
 
 		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
-		private static extern IntPtr RegisterDeviceNotification(IntPtr recipient, IntPtr notificationFilter, int flags);
+		private static extern IntPtr RegisterDeviceNotification(IntPtr recipient, IntPtr notificationFilter, RegisterDeviceNotificationFlags flags);
 
 		[DllImport("user32.dll")]
 		private static extern bool UnregisterDeviceNotification(IntPtr handle);
@@ -67,19 +69,16 @@ namespace MUNIA {
 		[StructLayout(LayoutKind.Sequential)]
 		private struct DevBroadcastDeviceinterface {
 			internal int Size;
-			internal int DeviceType;
+			internal DeviceType DeviceType;
 			internal int Reserved;
 			internal Guid ClassGuid;
 			internal short Name;
 		}
 
-
 		/// <summary>
 		/// Represents the window that is used internally to get the messages.
 		/// </summary>
 		sealed class UsbDeviceNotificationListenerWindow : NativeWindow, IDisposable {
-			const int DbtDeviceArrival = 0x8000; // system detected a new device        
-			const int DbtDeviceRemoveComplete = 0x8004; // device is gone      
 			const int WmDevicechange = 0x0219; // device change event      
 
 			public UsbDeviceNotificationListenerWindow() {
@@ -89,35 +88,35 @@ namespace MUNIA {
 
 			protected override void WndProc(ref Message m) {
 				base.WndProc(ref m);
-				if (m.Msg == WmDevicechange) {
+				if (m.Msg != WmDevicechange) return;
 
-					if ((int)m.WParam != DbtDeviceArrival && (int)m.WParam != DbtDeviceRemoveComplete)
-						return;
-					
-					string name = null;
+				WM_DeviceChangeMessage wp = (WM_DeviceChangeMessage)m.WParam;
+				if (wp != WM_DeviceChangeMessage.DBT_DEVICEARRIVAL && wp != WM_DeviceChangeMessage.DBT_DEVICEREMOVECOMPLETE)
+					return;
 
-					var hdr = (DeviceBroadcastHeader)Marshal.PtrToStructure(m.LParam, typeof(DeviceBroadcastHeader));
-					DeviceType type = hdr.dbcc_devicetype;
-					if (type == DeviceType.DBT_DEVTYP_PORT) {
-						var prt = (DEV_BROADCAST_PORT)Marshal.PtrToStructure(m.LParam, typeof(DEV_BROADCAST_PORT));
-						name = new string(prt.dbcp_name);
-					}
-					else if (type == DeviceType.DBT_DEVTYP_DEVICEINTERFACE) {
-						var dev = (DEV_BROADCAST_DEVICEINTERFACE)Marshal.PtrToStructure(m.LParam, typeof(DEV_BROADCAST_DEVICEINTERFACE));
-						name = new string(dev.dbcc_name);
-					}
-					name = name?.Substring(0, name.IndexOf('\0'));
+				Guid guid = Guid.Empty;
+				string name = null;
 
-					var args = new UsbNotificationEventArgs(type, name);
-					switch ((int)m.WParam) {
-						case DbtDeviceRemoveComplete:
-							OnDeviceRemoveComplete(args); //
-							break;
-						case DbtDeviceArrival:
-							OnDeviceArrival(args);
-							break;
-					}
+				var hdr = (DeviceBroadcastHeader)Marshal.PtrToStructure(m.LParam, typeof(DeviceBroadcastHeader));
+				DeviceType type = hdr.dbcc_devicetype;
+
+				if (type == DeviceType.DBT_DEVTYP_PORT) {
+					var prt = (DEV_BROADCAST_PORT)Marshal.PtrToStructure(m.LParam, typeof(DEV_BROADCAST_PORT));
+					name = new string(prt.dbcp_name);
 				}
+				else if (type == DeviceType.DBT_DEVTYP_DEVICEINTERFACE) {
+					var dev = (DEV_BROADCAST_DEVICEINTERFACE)Marshal.PtrToStructure(m.LParam, typeof(DEV_BROADCAST_DEVICEINTERFACE));
+					name = new string(dev.dbcc_name);
+					guid = dev.dbcc_classguid;
+				}
+				name = name?.Substring(0, name.IndexOf('\0'));
+
+				var args = new UsbNotificationEventArgs(type, name, guid);
+				if (wp == WM_DeviceChangeMessage.DBT_DEVICEARRIVAL)
+					OnDeviceArrival(args);
+				else
+					OnDeviceRemoveComplete(args);
+
 			}
 			public void Dispose() {
 				DestroyHandle();
@@ -129,9 +128,12 @@ namespace MUNIA {
 	public class UsbNotificationEventArgs : EventArgs {
 		public DeviceType DeviceType { get; private set; }
 		public string Name { get; private set; }
-		public UsbNotificationEventArgs(DeviceType type, string name) {
+		public Guid Guid { get; private set; }
+
+		public UsbNotificationEventArgs(DeviceType type, string name, Guid guid) {
 			DeviceType = type;
 			Name = name;
+			Guid = guid;
 		}
 	}
 
@@ -145,6 +147,28 @@ namespace MUNIA {
 		DBT_DEVTYP_HANDLE = 0x00000006 //File system handle
 	}
 
+	public enum WM_DeviceChangeMessage {
+		DBT_CONFIGCHANGECANCELED = 0x0019, // A request to change the current configuration (dock or undock) has been canceled.
+		DBT_CONFIGCHANGED = 0x0018, // The current configuration has changed, due to a dock or undock.
+		DBT_CUSTOMEVENT = 0x8006, // A custom event has occurred.
+		DBT_DEVICEARRIVAL = 0x8000, // A device or piece of media has been inserted and is now available.
+		DBT_DEVICEQUERYREMOVE = 0x8001, // Permission is requested to remove a device or piece of media. Any application can deny this request and cancel the removal.
+		DBT_DEVICEQUERYREMOVEFAILED = 0x8002, // A request to remove a device or piece of media has been canceled.
+		DBT_DEVICEREMOVECOMPLETE = 0x8004, // A device or piece of media has been removed.
+		DBT_DEVICEREMOVEPENDING = 0x8003, // A device or piece of media is about to be removed. Cannot be denied.
+		DBT_DEVICETYPESPECIFIC = 0x8005, // A device-specific event has occurred.
+		DBT_DEVNODES_CHANGED = 0x0007, // A device has been added to or removed from the system.
+		DBT_QUERYCHANGECONFIG = 0x0017, // Permission is requested to change the current configuration (dock or undock).
+		DBT_USERDEFINED = 0xFFFF, // The meaning of this message is user-defined.
+	}
+
+	[Flags]
+	public enum RegisterDeviceNotificationFlags : int {
+		DEVICE_NOTIFY_WINDOW_HANDLE = 0x00000000, // The hRecipient parameter is a window handle
+		DEVICE_NOTIFY_SERVICE_HANDLE = 0x00000001, // The hRecipient parameter is a service status handle
+		DEVICE_NOTIFY_ALL_INTERFACE_CLASSES = 0x00000004, // Notifies the recipient of device interface events for all device interface classes
+	}
+
 	[StructLayout(LayoutKind.Sequential, Pack = 1)]
 	public struct DeviceBroadcastHeader {
 		public Int32 dbcc_size;
@@ -154,11 +178,10 @@ namespace MUNIA {
 	
 	[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
 	public struct DEV_BROADCAST_DEVICEINTERFACE {
-		public Int32 dbcc_size;
-		public Int32 dbcc_devicetype;
-		public Int32 dbcc_reserved;
-		[MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.U1, SizeConst = 16)]
-		public byte[] dbcc_classguid;
+		public int dbcc_size;
+		public int dbcc_devicetype;
+		public int dbcc_reserved;
+		public Guid dbcc_classguid;
 		[MarshalAs(UnmanagedType.ByValArray, SizeConst = 128)]
 		public char[] dbcc_name;
 	}
