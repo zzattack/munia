3ee3060ec2d2a73af23c21da85a63a8e5ca934ab
Frank Razenberg <frank@zzattack.org>
2016-10-03 03:25:16 +0200

    tighten memory
    Former-commit-id: 935519d7d0fbb151815fc007cd7b0ce02189ed0c

--- MUNIA-asm.X/main.asm
+++ MUNIA-asm.X/main.asm
@@ -10,13 +10,21 @@ GMEM		equ		0x008
 pSW		equ		0x010   ; sample buffer ptr
 pSMEM		equ		0x700   ; sample buffer memory
     
+    ORG 0x1008
+    goto    ISR
+    
+; program memory layout:
+; isr: 3f00-3f42 --> 66
+; snes: 3f50-3f88 --> 56
+; n64: 3f90-3fb8  --> 40
+; ngc: 3fc0-3fe8 --> 40
+    ORG 0x3f00
 #include "isr.inc"
-		    
-    ORG 0x3a00
+    ORG 0x3f50
 #include "capture-ngc.inc"
-    ORG 0x3c00
+    ORG 0x3f90
 #include "capture-n64.inc"
-    ORG 0x3e00
+    ORG 0x3fc4
 #include "capture-snes.inc"
 		
     END
