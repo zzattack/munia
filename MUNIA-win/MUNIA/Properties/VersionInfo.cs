93025b4364a7dc48e3f7ca76f03afe4a9cd58d6b
Frank Razenberg <frank@zzattack.org>
2016-11-20 23:38:02 +0100

    use better tool for version auto-increment

--- MUNIA-win/MUNIA/Properties/VersionInfo.cs
+++ MUNIA-win/MUNIA/Properties/VersionInfo.cs
@@ -9,6 +9,6 @@
 //------------------------------------------------------------------------------
 
 [assembly: System.Reflection.AssemblyVersion("0.9")]
-[assembly: System.Reflection.AssemblyFileVersion("0.9.277.22")]
+[assembly: System.Reflection.AssemblyFileVersion("0.9.279.22")]
 
 
