b9ba257fb45be5aae0982fbb3c2b44b41621eaca
Frank Razenberg <frank@zzattack.org>
2017-04-02 00:52:38 +0200

    munia.io work

--- munia_io/tool/models.py
+++ munia_io/tool/models.py
@@ -3,10 +3,29 @@ from django.db import models
 class ProgramVersion(models.Model):
     version = models.CharField(max_length=100, unique=True)
     release_date = models.DateField(auto_now_add=True)
-    release_notes = models.CharField(max_length=100, unique=True)
+    release_notes = models.CharField(max_length=100)
     file = models.FileField(upload_to='versions')
-    
+
     def __unicode__ (self):
         return 'Version ' + self.version.__str__()
+
+    class Admin: pass
+
+class InputAdapterDevice(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+class FirmwareVersion(models.Model):
+    version = models.CharField(max_length=100, unique=True)
+    release_date = models.DateField(auto_now_add=True)
+    release_notes = models.CharField(max_length=100)
+    file = models.FileField(upload_to='versions')
+
+    device = models.ForeignKey(InputAdapterDevice)
+    supported_hw_revisions = models.CharField(max_length=100) # range for device hardware revision
+    supported_device_ids = models.CharField(max_length=100) # regex for microcontroller device id
+
+
+    def __unicode__ (self):
+        return 'FW Version ' + self.version.__str__()
 
     class Admin: pass
