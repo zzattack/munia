12fd71dbf2bdbf71bd2076322be928d1c62c09ad
Frank Razenberg <frank@zzattack.org>
2017-07-01 11:55:24 +0200

    static fix
    Former-commit-id: e2262812d262a11c0e2075dfecac4a0a596fc78e

--- munia_io/munia/settings.py
+++ munia_io/munia/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/www/munia.io/static'
-MEDIA_ROOT = '/www/munia.io/media/'
+STATIC_ROOT = '/www/munia.io/munia_io/static'
+MEDIA_ROOT = '/www/munia.io/munia_io/media/'
 MEDIA_URL = '/media/'
 
