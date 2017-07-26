501c1e1e761ec33d4225b4e4f8f27e10c16ad921
Frank Razenberg <frank@zzattack.org>
2017-07-26 15:31:23 +0200

    static fix

--- munia_io/config/settings_dev.py
+++ munia_io/config/settings_dev.py
@@ -109,13 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Amsterdam'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
