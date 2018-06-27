e61e3d6f575356d71e231982198139c1c935640c
Frank Razenberg <frank@zzattack.org>
2018-06-27 17:01:59 +0200

    poller framework
    Former-commit-id: 37f74f04a5b11642b713be8cc82be120cf0c7d93

--- firmware/MUSIA/ps2_poller.h
+++ firmware/MUSIA/ps2_poller.h
@@ -1,7 +1,41 @@
 #pragma once
 
+#include "hal_spi.h"
+
+enum class polling_interval {
+	poll25Hz = 25,
+	poll30Hz = 30,
+	poll50Hz = 50,
+	poll60Hz = 60,
+	poll100Hz = 100,
+	poll120Hz = 120,
+};
+
+enum class config_state {
+	notInitialized,
+	enterConfigMode,
+	turnOnAnalog,
+	setupMotorMapping,
+	enablePressureMappings,
+	exitConfig,
+	completed
+};
+
 class ps2_poller {
-public:
-	void configure();
+private:
+	polling_interval freq;
+	config_state state = config_state::notInitialized;
+	hal_spi_interface* spi;
+	void poll();
+	void spi_exchange(const uint8_t* w, uint8_t* r, uint16_t len);
+	
+public:	
+	ps2_poller(hal_spi_interface* spi);
+
+	void init();
+	void deInit();
+	void start(polling_interval freq);
+	void stop();
+
 	void work();
 };
