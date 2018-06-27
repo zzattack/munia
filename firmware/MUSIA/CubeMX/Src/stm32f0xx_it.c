58b3bec31a55fb8385182d84cd99e4a7ec324d66
Frank Razenberg <frank@zzattack.org>
2018-06-28 00:48:40 +0200

    sync

--- firmware/MUSIA/CubeMX/Src/stm32f0xx_it.c
+++ firmware/MUSIA/CubeMX/Src/stm32f0xx_it.c
@@ -73,7 +73,7 @@ void HardFault_Handler(void)
     /* USER CODE BEGIN W1_HardFault_IRQn 0 */
 	  sys_printf("HARDFAULT\n");
 	  while (1);
-	  /* USER CODE END W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
   }
   /* USER CODE BEGIN HardFault_IRQn 1 */
 
