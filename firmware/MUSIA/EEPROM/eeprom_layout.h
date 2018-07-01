258441b5f26ec53d1257b7b9387e6192dbca9493
Frank Razenberg <frank@zzattack.org>
2018-07-01 12:27:34 +0200

    make C compatible

--- firmware/MUSIA/EEPROM/eeprom_layout.h
+++ firmware/MUSIA/EEPROM/eeprom_layout.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #define EEPROM_SIZE sizeof(EELayout)
 
-
-enum class musia_mode { sniffer = 0, poller };
+typedef enum { MODE_SNIFFER = 0, MODE_POLLER, MODE_INVALID = 0xFF } musia_mode;
 
 typedef struct __attribute__((__packed__)) {
 	musia_mode mode;
@@ -14,7 +14,7 @@ typedef struct __attribute__((__packed__)) {
 } EELayout;
 
 const EELayout FactoryEEPROM = {
-	.mode =  musia_mode::sniffer,
+	.mode = MODE_SNIFFER,
 	.allowVibrate = true,
 	.pollFreq = 60,
 };
