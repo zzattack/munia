6aeeb607d58baede8991d65ed0e920e35f0858fb
Frank Razenberg <frank@zzattack.org>
2018-07-12 17:26:55 +0200

    color to hex string (ColorTranslator tends to pick non-hex names when possible)

--- MUNIA-win/MUNIA/Util/ExtensionMethods.cs
+++ MUNIA-win/MUNIA/Util/ExtensionMethods.cs
@@ -1,10 +1,18 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace MUNIA.Util {
 	static partial class ExtensionMethods {
 		public static void EnsureSize<T>(this List<T> list, int count) {
 			while (list.Count < count) list.Add(Activator.CreateInstance<T>());
 		}
+
+		public static string ToHexValue(this Color color) {
+			return "#" + color.R.ToString("X2") +
+					color.G.ToString("X2") +
+					color.B.ToString("X2");
+		}
+
 	}
 }
