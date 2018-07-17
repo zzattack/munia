1e5d4213948007c5dc36faf77483ca336600d56c
Frank Razenberg <frank@zzattack.org>
2018-07-17 11:59:41 +0200

    updated settings_dev, old apps removed, munia_io.db as sqlite name

--- munia_io/config/settings_dev.py
+++ munia_io/config/settings_dev.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -28,13 +27,12 @@ DEBUG = True
 ALLOWED_HOSTS = [ '*']
 
 
-
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('./', 'yourdatabasename.db'),
+        'NAME': os.path.join('./', 'munia_io.db'),
     }
 
 }
@@ -49,9 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'adapters',
-    'consoles',
     'home',
-    'repros',
     'tool',
 ]
 
