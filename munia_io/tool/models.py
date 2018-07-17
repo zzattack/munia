2159c6d3455614d8a682dfc3e06c49aa85004bb3
Frank Razenberg <frank@zzattack.org>
2018-07-17 11:58:53 +0200

    add required on_delete

--- munia_io/tool/models.py
+++ munia_io/tool/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-  
+
+
 class ProgramVersion(models.Model):
     version = models.CharField(max_length=100, unique=True)
     release_date = models.DateField(auto_now_add=True)
@@ -11,8 +12,10 @@ class ProgramVersion(models.Model):
 
     class Admin: pass
 
+
 class InputAdapterDevice(models.Model):
     name = models.CharField(max_length=100, unique=True)
+
 
 class FirmwareVersion(models.Model):
     version = models.CharField(max_length=100, unique=True)
@@ -20,10 +23,9 @@ class FirmwareVersion(models.Model):
     release_notes = models.CharField(max_length=100)
     file = models.FileField(upload_to='versions')
 
-    device = models.ForeignKey(InputAdapterDevice)
+    device = models.ForeignKey(InputAdapterDevice, on_delete=models.CASCADE)
     supported_hw_revisions = models.CharField(max_length=100) # range for device hardware revision
     supported_device_ids = models.CharField(max_length=100) # regex for microcontroller device id
-
 
     def __unicode__ (self):
         return 'FW Version ' + self.version.__str__()
