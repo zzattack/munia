d2b1feaae3f3f75022d6569411d2c8cec339e79d
Frank Razenberg <frank@zzattack.org>
2018-07-18 13:07:15 +0200

    devices page

--- munia_io/home/urls.py
+++ munia_io/home/urls.py
@@ -4,7 +4,6 @@ from home.views import *
 urlpatterns = [
 	url(r'^(?i)(?:index.*)?$', index, name='index'),
 	url(r'^(?i)contact$', contact, name='contact'),
-	url(r'^(?i)devices', contact, name='devices'),
+	url(r'^(?i)devices', devices, name='devices'),
 	url(r'^(?i)faq', faq, name='faq'),
-	url(r'^(?i)software', software, name='software'),
 ]
