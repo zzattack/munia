0f8ef1caa5ddcbd88bfd5a7fc1624af868ed3fb9
Frank Razenberg <frank@zzattack.org>
2018-08-01 15:02:40 +0200

    update middleware, enable admin

--- munia_io/config/settings_dev.py
+++ munia_io/config/settings_dev.py
@@ -49,15 +49,15 @@ INSTALLED_APPS = [
 	'tool',
 ]
 
-MIDDLEWARE_CLASSES = [
+MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.common.CommonMiddleware',
+	# 'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	# 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
 	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	# 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
--- munia_io/config/urls.py
+++ munia_io/config/urls.py
@@ -1,14 +1,16 @@
-from django.conf.urls import url, include
-from django.contrib import admin
 from django.conf import settings
+from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
 import adapters.urls
-import tool.urls
 import home.urls
+import tool.urls
 
 urlpatterns = [
 	url(r'^(?:adapters|munia)/', include(adapters.urls)),
 	url(r'^tool/', include(tool.urls)),
-	url(r'^admin/', admin.site.urls),
 	url(r'^', include(home.urls)),
+	path(r'admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
