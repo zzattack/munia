b913333a06ce2a43d903b38552a45fb7df70e83e
Frank Razenberg <frank@zzattack.org>
2018-08-04 23:03:57 +0200

    add analytical stub

--- munia_io/config/urls.py
+++ munia_io/config/urls.py
@@ -4,12 +4,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-import adapters.urls
 import home.urls
 import tool.urls
 
 urlpatterns = [
-	url(r'^(?:adapters|munia)/', include(adapters.urls)),
 	url(r'^tool/', include(tool.urls)),
 	url(r'^', include(home.urls)),
 	path(r'admin/', admin.site.urls),
