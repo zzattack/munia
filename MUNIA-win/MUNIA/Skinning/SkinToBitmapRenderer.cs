f46a0219354f308cd30cf5f349f517a241f21f63
Frank Razenberg <frank@zzattack.org>
2018-11-28 23:07:09 +0100

    render to requested size

--- MUNIA-win/MUNIA/Skinning/SkinToBitmapRenderer.cs
+++ MUNIA-win/MUNIA/Skinning/SkinToBitmapRenderer.cs
@@ -5,19 +5,13 @@ using OpenTK.Graphics.OpenGL;
 using PixelFormat = System.Drawing.Imaging.PixelFormat;
 
 namespace MUNIA.Skinning {
-	public class SkinToBitmapRenderer :IDisposable {
-		private readonly DrawingSurface _surface;
-
-
-		public SkinToBitmapRenderer(int width, int height) {
-			_surface = new DrawingSurface(width, height, PixelFormat.Format32bppArgb);
-		}
-
-		public Bitmap Render(Skin skin, Color background) {
-			SetupFrameBuffer();
+	public static class SkinToBitmapRenderer {
+		public static Bitmap Render(Skin skin, Size size, Color background) {
+			var surface = new DrawingSurface(size.Width, size.Height, PixelFormat.Format32bppArgb);
+			SetupFrameBuffer(size);
 			GL.PushAttrib(AttribMask.ViewportBit);
 			{
-				GL.Viewport(0, 0, _surface.Width, _surface.Height);
+				GL.Viewport(0, 0, surface.Width, surface.Height);
 				GL.ClearColor(background);
 				GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
@@ -27,26 +21,26 @@ namespace MUNIA.Skinning {
 				GL.MatrixMode(MatrixMode.Projection);
 				GL.LoadIdentity();
 				GL.Scale(1.0, -1.0, 1.0); // invert now because readpixels reads rows in flipped vertical order
-				GL.Ortho(0, _surface.Width, _surface.Height, 0, 0.0, 4.0);
+				GL.Ortho(0, surface.Width, surface.Height, 0, 0.0, 4.0);
 
 				// draw the preview
 				skin.Activate();
-				skin.Render(_surface.Width, _surface.Height, true);
+				skin.Render(surface.Width, surface.Height, true);
 
 				// read pixels back to surface
-				_surface.Lock();
-				GL.ReadPixels(0, 0, _surface.BitmapData.Width, _surface.BitmapData.Height, 
-					OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, _surface.BitmapData.Scan0);
-				_surface.Unlock();
+				surface.Lock();
+				GL.ReadPixels(0, 0, surface.BitmapData.Width, surface.BitmapData.Height,
+					OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, surface.BitmapData.Scan0);
+				surface.Unlock();
 			}
 			// deactivate framebuffer again
 			GL.PopAttrib();
 			GL.Ext.BindFramebuffer(FramebufferTarget.FramebufferExt, 0); // disable rendering into the FBO
 
-			return _surface.Bitmap;
+			return surface.Bitmap;
 		}
 
-		private bool SetupFrameBuffer() {
+		private static bool SetupFrameBuffer(Size size) {
 			try {
 				int fbo;
 				GL.Ext.GenFramebuffers(1, out fbo);
@@ -60,20 +54,17 @@ namespace MUNIA.Skinning {
 			int depthbuffer;
 			GL.Ext.GenRenderbuffers(1, out depthbuffer);
 			GL.Ext.BindRenderbuffer(RenderbufferTarget.RenderbufferExt, depthbuffer);
-			GL.Ext.RenderbufferStorage(RenderbufferTarget.RenderbufferExt, RenderbufferStorage.DepthComponent32, _surface.Width, _surface.Height);
+			GL.Ext.RenderbufferStorage(RenderbufferTarget.RenderbufferExt, RenderbufferStorage.DepthComponent32, size.Width, size.Height);
 			GL.Ext.FramebufferRenderbuffer(FramebufferTarget.FramebufferExt, FramebufferAttachment.DepthAttachmentExt, RenderbufferTarget.RenderbufferExt, depthbuffer);
 
 			int rgb_rb;
 			GL.Ext.GenRenderbuffers(1, out rgb_rb);
 			GL.Ext.BindRenderbuffer(RenderbufferTarget.RenderbufferExt, rgb_rb);
-			GL.Ext.RenderbufferStorage(RenderbufferTarget.RenderbufferExt, RenderbufferStorage.Rgba8, _surface.Width, _surface.Height);
+			GL.Ext.RenderbufferStorage(RenderbufferTarget.RenderbufferExt, RenderbufferStorage.Rgba8, size.Width, size.Height);
 			GL.Ext.FramebufferRenderbuffer(FramebufferTarget.FramebufferExt, FramebufferAttachment.ColorAttachment0Ext, RenderbufferTarget.RenderbufferExt, rgb_rb);
 
 			return GL.CheckFramebufferStatus(FramebufferTarget.FramebufferExt) == FramebufferErrorCode.FramebufferCompleteExt;
 		}
 
-		public void Dispose() {
-			_surface?.Dispose();
-		}
 	}
 }
