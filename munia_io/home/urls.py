1274d0b26e3ba3932ee1552590524e61e3e26fe7
Frank Razenberg <frank@zzattack.org>
2019-02-12 22:11:52 +0100

    make urls really reversible

--- munia_io/home/urls.py
+++ munia_io/home/urls.py
@@ -2,10 +2,10 @@ from django.urls import *
 from home.views import *
 
 urlpatterns = [
-	re_path(r'^(?:index.*)?(?i)$', index, name='index'),
-	re_path(r'^devices(?i)$', devices, name='devices'),
-	re_path(r'^software(?i)$', software, name='software'),
-	re_path(r'^faq(?i)$', faq, name='faq'),
-	re_path(r'^contact(?i)$', contact, name='contact'),
-	re_path(r'^success(?i)$', success, name='success'),
+	re_path(r'^(?:index.*)?$(?i)', index, name='index'),
+	re_path(r'^devices$(?i)', devices, name='devices'),
+	re_path(r'^software$(?i)', software, name='software'),
+	re_path(r'^faq$(?i)', faq, name='faq'),
+	re_path(r'^contact$(?i)', contact, name='contact'),
+	re_path(r'^success$(?i)', success, name='success'),
 ]
--- munia_io/tool/urls.py
+++ munia_io/tool/urls.py
@@ -3,7 +3,7 @@ from .views import *
 
 urlpatterns = [
 	re_path(r'^$', index),
-	re_path(r'^version_check(?i)$', version_check),
-	re_path(r'^get_latest(?i)$', get_latest),
-	re_path(r'^report_bug(?i)$', report_bug),
+	re_path(r'^version_check$(?i)', version_check),
+	re_path(r'^get_latest$(?i)', get_latest),
+	re_path(r'^report_bug$(?i)', report_bug),
 ]
