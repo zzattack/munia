95c8d0bb4391b7a331879aa47fd17a20a8fa56fd
Frank Razenberg <frank@zzattack.org>
2020-12-13 22:57:21 +0100

    updat build script for vs2019

--- MUNIA-win/Installer/build_release.bat
+++ MUNIA-win/Installer/build_release.bat
@@ -1,4 +1,4 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\Common7\Tools\VsDevCmd.bat"
 set MAKENSIS="%PROGRAMFILES(X86)%\nsis\makensis.exe"
 
 cd ..
